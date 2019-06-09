dd8ee6339e9284a183472c6476fdc6b18d3a1508
Jenn <jennchow215@gmail.com>
2019-06-09 14:10:53 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,13 @@
 # Define the variable board below.
+board=[" "]
+
+row = [" 0 ", " 1 ", " 2 ", " ", " ", " ", " "]
+
+#Position 1: Left 
+row [0]
+
+#Position 2: Middle 
+row [1]
+
+#Position 3: Right
+row [2]
